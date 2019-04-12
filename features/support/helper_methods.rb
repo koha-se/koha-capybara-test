447ba96a3d28b541016dba3c8d1fba38c2934761
Maria Hedberg <maria.hedberg@lub.lu.se>
2019-04-12 16:35:39 +0200

    Not sure why it was not working. Changing to this helped.

--- features/support/helper_methods.rb
+++ features/support/helper_methods.rb
@@ -3,6 +3,8 @@
 module LoginUser
   def login_user()
     visit(KOHA_TEST_OPAC_BASE + KOHA_TEST_OPAC_LOGIN_PAGE)
+    # debug to see what page we are on
+    expect(page).to have_content 'LUBcat Test'
     fill_in 'j_username', with: TEST_USER_USERNAME
     fill_in 'j_password', with: TEST_USER_PW
     click_button 'LOGGA IN'
